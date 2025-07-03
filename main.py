daf54061308b0b5c7290f2498def73c147e2de83
Karan Parashar <karan.parashar@samey.ai>
2025-07-04 00:17:50 +0530

    feat: Rewrite script

--- main.py
+++ main.py
@@ -1,49 +1,100 @@
-from github import Github
+from github import Github, UnknownObjectException
+from github.AuthenticatedUser import AuthenticatedUser
+from pydantic import BaseModel
+
+from typing import Tuple
 import os
+import logging
 import yaml
 
-
-def load_yaml(file_path):
-    with open(file_path, "r") as file:
-        return yaml.safe_load(file)
+logging.basicConfig(level=logging.INFO)
 
 
-def add_collaborators(data, org):
-    internal_team = set()
-    for role, users in data["internal_team"].items():
-        internal_team.update(users)
+class Project(BaseModel):
+    repos: list[str]
+    maintainers: list[str]
 
-    for project, details in data["projects"].items():
-        for repo_name in details["repos"]:
-            gh_repo = org.get_repo(repo_name)
 
-            # get current collaborators
-            current_collaborators = {
-                collab.login for collab in gh_repo.get_collaborators()
+class Auth:
+    def __init__(self, GITHUB_TOKEN):
+        self.github = Github(GITHUB_TOKEN)
+        self.org = self.github.get_organization("SDC-MUJ")
+
+    def update_project(self, project: Project, internal_team: list[str]) -> None:
+        for repo_meta in project.repos:
+            try:
+                repo = self.org.get_repo(repo_meta)
+            except UnknownObjectException:
+                # Create repo if not exist
+                logging.warning(f"Repo {repo_meta} does not exist. Creating ...")
+                repo = self.org.create_repo(name=repo_meta, private=True)
+
+            collaborators: set[str] = {
+                user.login.lower() for user in repo.get_collaborators()
             }
+            logging.debug(f"Current collaborators for {repo.name}: {collaborators}")
 
-            # get maintainers list
-            maintainers_list = details["team"] or []
-            maintainers_set = set(maintainers_list)
+            # Handle new additions
+            additions = set(project.maintainers) - (collaborators | set(internal_team))
+            for user in additions:
+                logging.info("Adding user %s to %s", user, repo.name)
+                repo.add_to_collaborators(user, permission="maintain")
 
-            # see if there's anyone to add
-            for maintainer in maintainers_set - current_collaborators - internal_team:
-                print(f"Adding {maintainer} as a maintainer to {repo_name}")
-                gh_repo.add_to_collaborators(maintainer, permission="write")
+            # Warn for removals of old maintainers
+            removals = collaborators - (set(project.maintainers) | set(internal_team))
+            for user in removals:
+                logging.warning("Removing user %s from %s", user, repo.name)
+                repo.remove_from_collaborators(user)
 
-            # see if there's anyone to remove
-            for collaborator in current_collaborators - maintainers_set - internal_team:
-                print(f"{collaborator} needs to be removed from {repo_name}")
-                # NOTE: This is a manual process, DO NOT MAKE THIS AUTOMATIC
-                # gh_repo.remove_from_collaborators(collaborator)
+    def update_org(self, maintainers: set[str]) -> None:
+        org_members = self.org.get_members()
+        org_members_list = {member.login.lower() for member in org_members}
+        logging.debug(f"Current organization members: {org_members_list}")
+
+        # Handle new additions
+        additions = maintainers - org_members_list
+
+        for member in additions:
+            user = self.github.get_user(member)
+            if isinstance(user, AuthenticatedUser):
+                continue
+            logging.info("Adding %s to organization", member)
+            self.org.invite_user(user, role="direct_member")
+
+
+def parse_and_flatten(projects_yaml: str) -> Tuple[list[Project], list[str], set[str]]:
+    with open(projects_yaml, "r") as f:
+        data = yaml.safe_load(f)
+        projects = []
+        project_maintainers = []
+        for _, project_meta in data["projects"].items():
+            repos = []
+            maintainers = []
+            for repo in project_meta["repos"]:
+                repos.append(repo)
+            for maintainer in project_meta["maintainers"] or []:
+                maintainers.append(maintainer.lower())
+            project_maintainers.extend(maintainers)
+            project = Project(repos=repos, maintainers=maintainers)
+            projects.append(project)
+
+        internal_team = []
+        for _, users in data["internal_team"].items():
+            for user in users:
+                internal_team.append(user.lower())
+
+        return projects, internal_team, set(project_maintainers)
 
 
 def main():
     GITHUB_TOKEN = os.environ["X_GITHUB_TOKEN"]
-    g = Github(GITHUB_TOKEN)
-    org = g.get_organization("SDC-MUJ")
-    data = load_yaml("projects.yaml")
-    add_collaborators(data, org)
+    auth = Auth(GITHUB_TOKEN)
+
+    projects, internal_team, maintainers = parse_and_flatten("projects.yaml")
+    for project in projects:
+        auth.update_project(project, internal_team)
+
+    auth.update_org(maintainers)
 
 
 if __name__ == "__main__":
